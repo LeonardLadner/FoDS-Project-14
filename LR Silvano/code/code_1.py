4943074c21deb54474c828a370ece12f9bfd6228
LeonardLadner <leonard.ladner2002@gmail.com>
2024-06-04 12:06:03 +0200

    LR: Implementation of subset

--- LR Silvano/code/code_1.py
+++ LR Silvano/code/code_1.py
@@ -27,11 +27,20 @@ y = data_encoded['HeartDisease_Yes']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+pos_patients = list(np.random.choice(X_train[y_train == 1].index, 1000))
+neg_patients = list(np.random.choice(X_train[y_train == 0].index, 1000))
+X_train_sub = X_train.loc[pos_patients+neg_patients]
+y_train_sub = y_train.loc[pos_patients+neg_patients]
+
+pos_patients_test = list(X_test[y_test == 1].index)
+neg_patients_test = list(np.random.choice(X_test[y_test == 0].index, len(pos_patients_test)))
+X_test_sub = X_test.loc[pos_patients_test+neg_patients_test]
+y_test_sub = y_test.loc[pos_patients_test+neg_patients_test]
 
 scaler = StandardScaler()
 numeric_features = X.select_dtypes(include=['float64', 'int']).columns.tolist()
-X_train[numeric_features] = scaler.fit_transform(X_train[numeric_features])
-X_test[numeric_features] = scaler.transform(X_test[numeric_features])
+X_train_sub[numeric_features] = scaler.fit_transform(X_train_sub[numeric_features])
+X_test_sub[numeric_features] = scaler.transform(X_test_sub[numeric_features])
 
 
 param_grid = {
@@ -44,20 +53,20 @@ param_grid = {
 grid_search = GridSearchCV(estimator=LogisticRegression(), param_grid=param_grid, cv=5, scoring='accuracy')
 
 
-grid_search.fit(X_train, y_train)
+grid_search.fit(X_train_sub, y_train_sub)
 
 
 print("Best parameters:", grid_search.best_params_)
 print("Best cross-validation score: {:.2f}".format(grid_search.best_score_))
 
 
-y_pred = grid_search.predict(X_test)
-y_pred_prob = grid_search.predict_proba(X_test)[:, 1]
+y_pred = grid_search.predict(X_test_sub)
+y_pred_prob = grid_search.predict_proba(X_test_sub)[:, 1]
 
 
-accuracy = accuracy_score(y_test, y_pred)
-conf_matrix = confusion_matrix(y_test, y_pred)
-report = classification_report(y_test, y_pred)
+accuracy = accuracy_score(y_test_sub, y_pred)
+conf_matrix = confusion_matrix(y_test_sub, y_pred)
+report = classification_report(y_test_sub, y_pred)
 print("Accuracy:", accuracy)
 print("Confusion Matrix:\n", conf_matrix)
 print("Classification Report:\n", report)
@@ -66,7 +75,7 @@ print("Classification Report:\n", report)
 
 
 if 'l1' in grid_search.best_params_['penalty']:  
-    coefficients = pd.DataFrame(grid_search.best_estimator_.coef_.flatten(), index=X_train.columns, columns=['Coefficients'])
+    coefficients = pd.DataFrame(grid_search.best_estimator_.coef_.flatten(), index=X_train_sub.columns, columns=['Coefficients'])
     coefficients = coefficients.sort_values(by='Coefficients', ascending=False)
     plt.figure(figsize=(20, 10))
     sns.barplot(x=coefficients['Coefficients'], y=coefficients.index)
@@ -83,8 +92,8 @@ plt.savefig("../output/ConfusionMatrix.png")
 
 
 
-fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
-roc_auc = roc_auc_score(y_test, y_pred_prob)
+fpr, tpr, thresholds = roc_curve(y_test_sub, y_pred_prob)
+roc_auc = roc_auc_score(y_test_sub, y_pred_prob)
 plt.figure(figsize=(8, 6))
 plt.plot(fpr, tpr, color='Blue', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
 plt.plot([0, 1], [0, 1], color='red', lw=2, linestyle='--')
